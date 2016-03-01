42bcdf1ebf04d0f18128984d5398f8f0f289811c
Marius Tomaschewski <mt@suse.de>
2016-03-01 10:18:39 +0100

    defined rumcay job matrix override

---
+++ jenkins-files/rumcay/config-job.sh
@@ -0,0 +1,163 @@
+#
+# Job definitions used on rumcay
+#
+NAME="sle12-0-last-direct"
+DISTRIBUTION="SLES 12 SP0 (x86_64)"
+BRANCH_NAME="sle12"
+NANNY="without"
+ID=1 configure
+
+NAME="sle12-0-last-nanny"
+DISTRIBUTION="SLES 12 SP0 (x86_64)"
+BRANCH_NAME="sle12"
+NANNY="with"
+ID=2 configure
+
+NAME="sle12-0-next-direct"
+DISTRIBUTION="SLES 12 SP0 (x86_64)"
+BRANCH_NAME="sle12-next"
+NANNY="without"
+ID=3 configure
+
+NAME="sle12-0-next-nanny"
+DISTRIBUTION="SLES 12 SP0 (x86_64)"
+BRANCH_NAME="sle12-next"
+NANNY="with"
+ID=4 configure
+
+NAME="sle12-1-last-direct"
+DISTRIBUTION="SLES 12 SP1 (x86_64)"
+BRANCH_NAME="sle12-sp1"
+NANNY="without"
+ID=5 configure
+
+NAME="sle12-1-last-nanny"
+DISTRIBUTION="SLES 12 SP1 (x86_64)"
+BRANCH_NAME="sle12-sp1"
+NANNY="with"
+ID=6 configure
+
+NAME="sle12-1-next-direct"
+DISTRIBUTION="SLES 12 SP1 (x86_64)"
+BRANCH_NAME="sle12-sp1-next"
+NANNY="without"
+ID=7 configure
+
+NAME="sle12-1-next-nanny"
+DISTRIBUTION="SLES 12 SP1 (x86_64)"
+BRANCH_NAME="sle12-sp1-next"
+NANNY="with"
+ID=8 configure
+
+NAME="sle12-0-master-direct"
+DISTRIBUTION="SLES 12 SP0 (x86_64)"
+BRANCH_NAME="master"
+NANNY="without"
+ID=9 configure
+
+NAME="sle12-0-master-nanny"
+DISTRIBUTION="SLES 12 SP0 (x86_64)"
+BRANCH_NAME="master"
+NANNY="with"
+ID=10 configure
+
+NAME="sle12-1-master-direct"
+DISTRIBUTION="SLES 12 SP1 (x86_64)"
+BRANCH_NAME="master"
+NANNY="without"
+ID=11 configure
+
+NAME="sle12-1-master-nanny"
+DISTRIBUTION="SLES 12 SP1 (x86_64)"
+BRANCH_NAME="master"
+NANNY="with"
+ID=12 configure
+
+NAME="tumbleweed-master-direct"
+DISTRIBUTION="openSUSE Tumbleweed (x86_64)"
+BRANCH_NAME="master"
+NANNY="without"
+ID=13 configure
+
+NAME="tumbleweed-master-nanny"
+DISTRIBUTION="openSUSE Tumbleweed (x86_64)"
+BRANCH_NAME="master"
+NANNY="with"
+ID=14 configure
+
+NAME="sle12-0-testing-direct"
+DISTRIBUTION="SLES 12 SP0 (x86_64)"
+BRANCH_NAME="testing"
+NANNY="without"
+ID=15 configure
+
+NAME="sle12-0-testing-nanny"
+DISTRIBUTION="SLES 12 SP0 (x86_64)"
+BRANCH_NAME="testing"
+NANNY="with"
+ID=16 configure
+
+NAME="sle12-1-testing-direct"
+DISTRIBUTION="SLES 12 SP1 (x86_64)"
+BRANCH_NAME="testing"
+NANNY="without"
+ID=17 configure
+
+NAME="sle12-1-testing-nanny"
+DISTRIBUTION="SLES 12 SP1 (x86_64)"
+BRANCH_NAME="testing"
+NANNY="with"
+ID=18 configure
+
+NAME="tumbleweed-testing-direct"
+DISTRIBUTION="openSUSE Tumbleweed (x86_64)"
+BRANCH_NAME="testing"
+NANNY="without"
+ID=19 configure
+
+NAME="tumbleweed-testing-nanny"
+DISTRIBUTION="openSUSE Tumbleweed (x86_64)"
+BRANCH_NAME="testing"
+NANNY="with"
+ID=20 configure
+
+NAME="os132-master-direct"
+DISTRIBUTION="openSUSE 13.2 (x86_64)"
+BRANCH_NAME="master"
+NANNY="without"
+ID=21 configure
+
+NAME="os132-master-nanny"
+DISTRIBUTION="openSUSE 13.2 (x86_64)"
+BRANCH_NAME="master"
+NANNY="with"
+ID=22 configure
+
+NAME="os132-testing-direct"
+DISTRIBUTION="openSUSE 13.2 (x86_64)"
+BRANCH_NAME="testing"
+NANNY="without"
+ID=23 configure
+
+NAME="os132-testing-nanny"
+DISTRIBUTION="openSUSE 13.2 (x86_64)"
+BRANCH_NAME="testing"
+NANNY="with"
+ID=24 configure
+
+NAME="opensuse-leap-master-direct"
+DISTRIBUTION="openSUSE Leap 42.1 (x86_64)"
+BRANCH_NAME="master"
+NANNY="without"
+ID=25 configure
+
+NAME="opensuse-leap-master-nanny"
+DISTRIBUTION="openSUSE Leap 42.1 (x86_64)"
+BRANCH_NAME="master"
+NANNY="with"
+ID=26 configure
+
+###
+# User job IDs 30..39, 40..49
+###
+
